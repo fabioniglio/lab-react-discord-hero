3554c116967dbb2a842f951839239d325d3d8db9
Fabio Niglio <fabio_niglio@hotmail.com>
2024-04-01 16:48:39 +0200

    Side items

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Navbar from "./Navbar";
 
 import discordLogo from "./assets/discord-logo-white.png";
-import menuIcon from "./assets/menu-icon.png"; // Import the contact icon
+import menuIcon from "./assets/menu-icon.png";
 
 function App() {
   const navItems = [
@@ -18,8 +18,17 @@ function App() {
   return (
     <div className="App">
       <Navbar className="navbar" navItems={navItems} />
-      <div className="heading">Test</div>
-      <div className="paragraph">Test</div>
+      <div className="description">
+        <div className="heading">IMAGINE A PLACE...</div>
+        <div className="paragraph">
+          ...where you can belong to a school club, a gaming group, or a
+          worldwide art community. Where just you and a handful of friends can
+          spend time together. a Place that makes it easy to talk every day and
+          hang out more often.
+        </div>
+        <button className="buttonMac">Download for Mac</button>
+        <button className="buttonBrowser">Open Discord in your browser</button>
+      </div>
     </div>
   );
 }
